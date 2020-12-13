af31747f6cfed424b061ae28f65c321bf5815f0e
kenge <pgkusn@gmail.com>
2020-12-13 16:07:31 +0800

    add https

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,12 @@
+const fs = require('fs');
+const path = require('path');
+
 module.exports = {
-    lintOnSave: false
+    lintOnSave: false,
+    devServer: {
+        https: true,
+        key: fs.readFileSync(path.resolve(__dirname, 'localhost-key.pem')),
+        cert: fs.readFileSync(path.resolve(__dirname, 'localhost.pem')),
+        host: 'localhost'
+    }
 };
