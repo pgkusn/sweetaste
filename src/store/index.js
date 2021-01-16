26a9ec8155d13c66c9c6943acd7b8994ea122b01
kenge <pgkusn@gmail.com>
2021-01-17 06:32:19 +0800

    store 模組化(修改路徑)

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,11 @@ import { createStore } from 'vuex';
 import NProgress from 'nprogress';
 import 'nprogress/nprogress.css';
 import axios from 'axios';
-import login from './login';
-import cart from './cart';
-import product from './product';
-import favorite from './favorite';
-import order from './order';
+import login from './modules/login';
+import cart from './modules/cart';
+import product from './modules/product';
+import favorite from './modules/favorite';
+import order from './modules/order';
 
 axios.defaults.baseURL = process.env.VUE_APP_API_URL;
 
