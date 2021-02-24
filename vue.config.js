ded1b032714a388b9a22709a85901e0b6f72112f
kenge <pgkusn@gmail.com>
2021-02-24 14:43:45 +0800

    改部署至 Firebase Hosting

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 module.exports = {
     lintOnSave: false,
-    publicPath: process.env.NODE_ENV === 'prod' ? '/sweetaste/' : '/',
+    // publicPath: process.env.NODE_ENV === 'prod' ? '/sweetaste/' : '/', // for github pages
     devServer: {
         https: true,
         key: fs.readFileSync(path.resolve(__dirname, 'localhost-key.pem')),
