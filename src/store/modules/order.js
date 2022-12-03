6b5c499a58f1e543c9d6086631868535b46f03e9
Kenge <pgkusn@gmail.com>
2022-12-03 19:54:32 +0800

    Modify api url

--- src/store/modules/order.js
+++ src/store/modules/order.js
@@ -1,16 +1,12 @@
-import axios from 'axios';
 import API from '@/data/api.js';
-
-const orderApi = axios.create({
-    baseURL: 'https://kenge-mock-api.herokuapp.com/sweetaste'
-});
+import axios from 'axios';
 
 export default {
     namespaced: true,
     actions: {
         async getOrderList (context, uid) {
             try {
-                const { data } = await orderApi({
+                const { data } = await axios({
                     method: API.orderList.method,
                     url: `${API.orderList.url}/${uid}`
                 });
@@ -29,7 +25,7 @@ export default {
         },
         async order (context, payload) {
             try {
-                const { data } = await orderApi({
+                const { data } = await axios({
                     method: API.order.method,
                     url: API.order.url,
                     data: payload
